ab84f2255316910f243a8fd78e088cbda03534d5
Andreia Pena <andreia.pena@pix.fr>
2023-08-01 13:51:28 +0200

    api: remove check-reconciliation step

--- api/tests/unit/domain/usecases/reconcile-oidc-user_test.js
+++ api/tests/unit/domain/usecases/reconcile-oidc-user_test.js
@@ -1,16 +1,21 @@
 import { expect, sinon, catchErr } from '../../../test-helper.js';
 import { reconcileOidcUser } from '../../../../lib/domain/usecases/reconcile-oidc-user.js';
-import { AuthenticationKeyExpired, MissingUserAccountError } from '../../../../lib/domain/errors.js';
-import { AuthenticationMethod } from '../../../../lib/domain/models/AuthenticationMethod.js';
+import { AuthenticationKeyExpired } from '../../../../lib/domain/errors.js';
+import { AuthenticationMethod } from '../../../../lib/domain/models/index.js';
 
 describe('Unit | UseCase | reconcile-oidc-user', function () {
-  let authenticationMethodRepository, userRepository, authenticationSessionService, oidcAuthenticationService;
+  let authenticationMethodRepository,
+    userRepository,
+    authenticationSessionService,
+    oidcAuthenticationService,
+    pixAuthenticationService;
   const identityProvider = 'POLE_EMPLOI';
 
   beforeEach(function () {
-    authenticationMethodRepository = { create: sinon.stub() };
+    authenticationMethodRepository = { create: sinon.stub(), findByUserId: sinon.stub() };
     userRepository = { updateLastLoggedAt: sinon.stub() };
     authenticationSessionService = { getByKey: sinon.stub() };
+    pixAuthenticationService = { getUserByUsernameAndPassword: sinon.stub() };
     oidcAuthenticationService = {
       identityProvider,
       createAccessToken: sinon.stub(),
@@ -22,9 +27,11 @@ describe('Unit | UseCase | reconcile-oidc-user', function () {
   it('should retrieve user session content', async function () {
     // given
     const sessionContent = { idToken: 'idToken' };
+    authenticationMethodRepository.findByUserId.resolves([]);
+    pixAuthenticationService.getUserByUsernameAndPassword.resolves({ id: 2 });
     authenticationSessionService.getByKey.resolves({
       sessionContent,
-      userInfo: { userId: 1, externalIdentityId: 'external_id', firstName: 'Anne' },
+      userInfo: { externalIdentityId: 'external_id', firstName: 'Anne' },
     });
     oidcAuthenticationService.createAuthenticationComplement
       .withArgs({ sessionContent })
@@ -37,6 +44,7 @@ describe('Unit | UseCase | reconcile-oidc-user', function () {
       authenticationKey: 'authenticationKey',
       oidcAuthenticationService,
       authenticationSessionService,
+      pixAuthenticationService,
       authenticationMethodRepository,
       userRepository,
     });
@@ -45,14 +53,54 @@ describe('Unit | UseCase | reconcile-oidc-user', function () {
     expect(authenticationSessionService.getByKey).to.be.calledOnceWith('authenticationKey');
   });
 
+  it('should find user and his authentication methods', async function () {
+    // given
+    const email = 'sarah.pix@example.net';
+    const password = 'pix123';
+    const sessionContent = { idToken: 'idToken' };
+    authenticationSessionService.getByKey.resolves({
+      sessionContent,
+      userInfo: { externalIdentityId: 'external_id', firstName: 'Sarah' },
+    });
+    pixAuthenticationService.getUserByUsernameAndPassword.resolves({ id: 2 });
+    authenticationMethodRepository.findByUserId.resolves([]);
+    oidcAuthenticationService.createAuthenticationComplement
+      .withArgs({ sessionContent })
+      .returns(
+        new AuthenticationMethod.OidcAuthenticationComplement({ accessToken: 'accessToken', expiredDate: new Date() })
+      );
+
+    // when
+    await reconcileOidcUser({
+      authenticationKey: 'authenticationKey',
+      email,
+      password,
+      oidcAuthenticationService,
+      authenticationSessionService,
+      pixAuthenticationService,
+      authenticationMethodRepository,
+      userRepository,
+    });
+
+    // then
+    expect(pixAuthenticationService.getUserByUsernameAndPassword).to.be.calledOnceWith({
+      username: email,
+      password,
+      userRepository,
+    });
+    expect(authenticationMethodRepository.findByUserId).to.be.calledOnceWith({ userId: 2 });
+  });
+
   it('should create authentication method with complement', async function () {
     // given
     const sessionContent = { idToken: 'idToken' };
     const externalIdentifier = 'external_id';
     const userId = 1;
+    authenticationMethodRepository.findByUserId.resolves([]);
+    pixAuthenticationService.getUserByUsernameAndPassword.resolves({ id: 1 });
     authenticationSessionService.getByKey.resolves({
       sessionContent,
-      userInfo: { userId, externalIdentityId: externalIdentifier, firstName: 'Anne' },
+      userInfo: { externalIdentityId: externalIdentifier, firstName: 'Anne' },
     });
     oidcAuthenticationService.createAuthenticationComplement
       .withArgs({ sessionContent })
@@ -65,6 +113,7 @@ describe('Unit | UseCase | reconcile-oidc-user', function () {
       authenticationKey: 'authenticationKey',
       oidcAuthenticationService,
       authenticationSessionService,
+      pixAuthenticationService,
       authenticationMethodRepository,
       userRepository,
     });
@@ -85,8 +134,10 @@ describe('Unit | UseCase | reconcile-oidc-user', function () {
     const userId = 1;
     authenticationSessionService.getByKey.resolves({
       sessionContent,
-      userInfo: { userId, externalIdentityId: externalIdentifier, firstName: 'Anne' },
+      userInfo: { externalIdentityId: externalIdentifier, firstName: 'Anne' },
     });
+    authenticationMethodRepository.findByUserId.resolves([]);
+    pixAuthenticationService.getUserByUsernameAndPassword.resolves({ id: 1 });
     oidcAuthenticationService.createAuthenticationComplement
       .withArgs({ sessionContent })
       .returns(
@@ -102,6 +153,7 @@ describe('Unit | UseCase | reconcile-oidc-user', function () {
       authenticationKey: 'authenticationKey',
       oidcAuthenticationService,
       authenticationSessionService,
+      pixAuthenticationService,
       authenticationMethodRepository,
       userRepository,
     });
@@ -133,28 +185,6 @@ describe('Unit | UseCase | reconcile-oidc-user', function () {
       // then
       expect(error).to.be.instanceOf(AuthenticationKeyExpired);
       expect(error.message).to.be.equal('This authentication key has expired.');
-    });
-  });
-
-  context('when user id is not stored in session info', function () {
-    it('should throw an MissingUserAccountError', async function () {
-      // given
-      authenticationSessionService.getByKey.resolves({
-        sessionContent: { idToken: 'idToken' },
-      });
-
-      // when
-      const error = await catchErr(reconcileOidcUser)({
-        authenticationKey: 'authenticationKey',
-        oidcAuthenticationService,
-        authenticationSessionService,
-        authenticationMethodRepository,
-        userRepository,
-      });
-
-      // then
-      expect(error).to.be.instanceOf(MissingUserAccountError);
-      expect(error.message).to.be.equal('Les informations de compte requises sont manquantes');
     });
   });
 });
